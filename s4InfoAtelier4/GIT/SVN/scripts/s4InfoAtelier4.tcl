2b59d0268de3d734a6a622a9b993575b1cf42b00
WillRob <william.roberge.wr@gmail.com>
2024-03-06 21:46:29 -0500

    fix first script

--- s4InfoAtelier4/GIT/SVN/scripts/s4InfoAtelier4.tcl
+++ s4InfoAtelier4/GIT/SVN/scripts/s4InfoAtelier4.tcl
@@ -39,11 +39,10 @@
 proc checkRequiredFiles { origin_dir} {
   set status true
   set files [list \
-   "C:/Travail/s4InfoAtelier4/s4InfoAtelier4.srcs/sources_1/imports/new/testPatternGen2.vhd" \
-   "C:/Travail/s4InfoAtelier4/s4InfoAtelier4.srcs/sources_1/bd/atelier4/atelier4.bd" \
-   "C:/Travail/s4InfoAtelier4/s4InfoAtelier4.srcs/sources_1/imports/atelier4_wrapper.vhd" \
-   "C:/Travail/s4InfoAtelier4/archive_project_summary.txt" \
-   "C:/Travail/s4InfoAtelier4/s4InfoAtelier4.srcs/constrs_1/imports/new/atelier4Constraints.xdc" \
+   "$origin_dir/../vhdSources/testPatternGen2.vhd" \
+   "$origin_dir/../vhdSources/atelier4_wrapper.vhd" \
+   "$origin_dir/../vhdSources/archive_project_summary.txt" \
+   "$origin_dir/../vhdSources/atelier4Constraints.xdc" \
   ]
   foreach ifile $files {
     if { ![file isfile $ifile] } {
@@ -53,10 +52,12 @@ proc checkRequiredFiles { origin_dir} {
   }
 
   set paths [list \
-   [file normalize "$origin_dir/../../../s4InfoAtelier4.ipdefs/mycolorRegister_1.0"] \
-   [file normalize "$origin_dir/../../../s4InfoAtelier4.ipdefs/pixelDataToVideoStream_1.0"] \
-   [file normalize "$origin_dir/../../../s4InfoAtelier4.ipdefs/rgb2dvi"] \
-   [file normalize "$origin_dir/../../../s4InfoAtelier4.ipdefs/tmds_v1_0"] \
+   [file normalize "$origin_dir/../ipRepo/mycolorRegister_1.0"] \
+   [file normalize "$origin_dir/../ipRepo/pixelDataToVideoStream_1.0"] \
+   [file normalize "$origin_dir/../ipRepo/rgb2dvi"] \
+   [file normalize "$origin_dir/../ipRepo/tmds_v1_0"] \
+   [file normalize "$origin_dir/../ipRepo/atelier4"] \
+   [file normalize "$origin_dir/../ipRepo/testPatternGen2"] \
   ]
   foreach ipath $paths {
     if { ![file isdirectory $ipath] } {
@@ -68,12 +69,18 @@ proc checkRequiredFiles { origin_dir} {
   return $status
 }
 # Set the reference directory for source file relative paths (by default the value is script directory path)
+# ==> Pointe vers le répertoire dans lequel on a mis les scripts TCL
 set origin_dir "."
 
 # Use origin directory path location variable, if specified in the tcl shell
 if { [info exists ::origin_dir_loc] } {
   set origin_dir $::origin_dir_loc
 }
+
+# ==> Créer le répertoire work dans lequel on va générer le projet Vivado
+# ==> Si le dossier work existe déjà le supprimer 
+file delete -force $origin_dir/../work
+file mkdir $origin_dir/../work/
 
 # Set the project name
 set _xil_proj_name_ "s4InfoAtelier4"
@@ -132,7 +139,7 @@ if { $::argc > 0 } {
 }
 
 # Set the directory path for the original project from where this script was exported
-set orig_proj_dir "[file normalize "$origin_dir/../../../"]"
+set orig_proj_dir "[file normalize "$origin_dir/../work/s4InfoAtelier4"]"
 
 # Check for paths and files needed for project creation
 set validate_required 0
@@ -146,7 +153,7 @@ if { $validate_required } {
 }
 
 # Create project
-create_project ${_xil_proj_name_} ./${_xil_proj_name_} -part xc7z010clg400-1
+create_project ${_xil_proj_name_} $origin_dir/../work/${_xil_proj_name_} -part xc7z010clg400-1
 
 # Set the directory path for the new project
 set proj_dir [get_property directory [current_project]]
@@ -181,7 +188,7 @@ if {[string equal [get_filesets -quiet sources_1] ""]} {
 # Set IP repository paths
 set obj [get_filesets sources_1]
 if { $obj != {} } {
-   set_property "ip_repo_paths" "[file normalize "$origin_dir/../../../s4InfoAtelier4.ipdefs/mycolorRegister_1.0"] [file normalize "$origin_dir/../../../s4InfoAtelier4.ipdefs/pixelDataToVideoStream_1.0"] [file normalize "$origin_dir/../../../s4InfoAtelier4.ipdefs/rgb2dvi"] [file normalize "$origin_dir/../../../s4InfoAtelier4.ipdefs/tmds_v1_0"]" $obj
+   set_property "ip_repo_paths" "[file normalize "$origin_dir/../ipRepo/mycolorRegister_1.0"] [file normalize "$origin_dir/../ipRepo/pixelDataToVideoStream_1.0"] [file normalize "$origin_dir/../ipRepo/rgb2dvi"] [file normalize "$origin_dir/../ipRepo/tmds_v1_0"]" $obj
 
    # Rebuild user ip_repo's index before adding any source files
    update_ip_catalog -rebuild
@@ -191,26 +198,29 @@ if { $obj != {} } {
 set obj [get_filesets sources_1]
 # Import local files from the original project
 set files [list \
- [file normalize "${origin_dir}/../../../s4InfoAtelier4.srcs/sources_1/imports/new/testPatternGen2.vhd"]\
- [file normalize "${origin_dir}/../../../s4InfoAtelier4.srcs/sources_1/bd/atelier4/atelier4.bd"]\
- [file normalize "${origin_dir}/../../../s4InfoAtelier4.srcs/sources_1/imports/atelier4_wrapper.vhd"]\
- [file normalize "${origin_dir}/../../../archive_project_summary.txt" ]\
+ [file normalize "${origin_dir}/../vhdSources/testPatternGen2.vhd"]\
+ [file normalize "${origin_dir}/../vhdSources/atelier4_wrapper.vhd"]\
+ [file normalize "${origin_dir}/../vhdSources/archive_project_summary.txt"]\
 ]
-set imported_files [import_files -fileset sources_1 $files]
+#set imported_files [import_files -fileset sources_1 $files]
+# ==>Ne pas créer de copies locales des fichiers sources dans le projet Vivado et garder une seule et unique version 
+add_files -norecurse -fileset $obj $files
 
 # Set 'sources_1' fileset file properties for remote files
 # None
 
 # Set 'sources_1' fileset file properties for local files
-set file "new/testPatternGen2.vhd"
+set file "$origin_dir/../vhdSources/testPatternGen2.vhd"
+set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "VHDL" -objects $file_obj
 
-set file "atelier4/atelier4.bd"
-set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
-set_property -name "registered_with_manager" -value "1" -objects $file_obj
+#set file "atelier4/atelier4.bd"
+#set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
+#set_property -name "registered_with_manager" -value "1" -objects $file_obj
 
-set file "imports/atelier4_wrapper.vhd"
+set file "$origin_dir/../vhdSources/atelier4_wrapper.vhd"
+set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "VHDL" -objects $file_obj
 
@@ -228,9 +238,10 @@ if {[string equal [get_filesets -quiet constrs_1] ""]} {
 set obj [get_filesets constrs_1]
 
 # Add/Import constrs file and set constrs file properties
-set file "[file normalize ${origin_dir}/../../../s4InfoAtelier4.srcs/constrs_1/imports/new/atelier4Constraints.xdc]"
-set file_imported [import_files -fileset constrs_1 [list $file]]
-set file "new/atelier4Constraints.xdc"
+set file "[file normalize ${origin_dir}/../constraints/atelier4Constraints.xdc]"
+set file_added [add_files -norecurse -fileset $obj [list $file]]
+set file "$origin_dir/../constraints/atelier4Constraints.xdc"
+set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets constrs_1] [list "*$file"]]
 set_property -name "file_type" -value "XDC" -objects $file_obj
 
@@ -554,3 +565,18 @@ move_dashboard_gadget -name {drc_1} -row 2 -col 0
 move_dashboard_gadget -name {timing_1} -row 0 -col 1
 move_dashboard_gadget -name {utilization_2} -row 1 -col 1
 move_dashboard_gadget -name {methodology_1} -row 2 -col 1
+
+# ###################### #
+# Créer le  block design
+# ###################### #
+source $origin_dir/../scripts/design_1.tcl
+
+# Générer le HDL wrapper du block design
+set design_name [get_bd_designs]
+make_wrapper -files [get_files $design_name.bd] -top -import
+
+# Regénérer le layout du block design
+regenerate_bd_layout
+
+# Sauvegarder le block design
+save_bd_design
